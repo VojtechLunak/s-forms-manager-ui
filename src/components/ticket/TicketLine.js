0d99472d362ed49e0b9fe76adc33adab8807d66f
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2025-05-05 13:50:05 +0200

    upd(ticket resolve modal)

--- src/components/ticket/TicketLine.js
+++ src/components/ticket/TicketLine.js
@@ -5,27 +5,50 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import API from "../../api";
+import {Alert, Modal} from "react-bootstrap";
 
 export class TicketLine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showConfirmModal: false,
+            isProcessing: false,
+            alertMessage: null,
+            alertVariant: null
+        }
+    }
+
     extractShortId(url) {
         const match = url.match(/\/(\d+)-/);
         return match ? match[1] : null;
     }
 
-    handleResolveAndOpenRecord() {
+    handleResolveAndOpenRecord = () => {
+        this.setState({isProcessing: true});
         const shortId = this.extractShortId(this.props.url);
 
         API.get(`/rest/ticket/resolve?ticketId=${shortId}&formGenUri=${this.props.contextUri}&projectName=${this.props.projectName}`
         ).then(response => {
             if (response.status === 200) {
-                alert("Issue resolved and Record set to 'open' state.");
+                this.setState({ isProcessing: false, alertMessage: "Issue resolved and Record set to 'open' state.", alertVariant: "success", showConfirmModal: false });
             } else {
-                alert("Failed to resolve issue and set Record to 'open' state.");
+                this.setState({ isProcessing: false, alertMessage: "An error occured when trying to resolve issue and open record.", alertVariant: "danger", showConfirmModal: false });
             }
         }).catch(error => {
-            console.error("Error while opening record:", error);
-            alert("Unexpected error.");
+            this.setState({ isProcessing: false, alertMessage: "An error occured when trying to resolve issue and open record.", alertVariant: "danger" });
         });
+    }
+
+    handleResolveIssue = () => {
+        this.setState({showConfirmModal: true});
+    }
+
+    handleCancelModal = () => {
+        this.setState({showConfirmModal: false});
+    };
+
+    handleAlertClose = () => {
+        this.setState({alertMessage: null, alertVariant: null});
     }
 
     render() {
@@ -40,6 +63,15 @@ export class TicketLine extends React.Component {
                                     <span><b>{this.props.name}</b> (<a href={this.props.url}
                                                                              target="_blank">link</a>)</span>
                                     <br/>
+                                    {this.state.alertMessage && (
+                                        <Row className="mt-3">
+                                            <Col>
+                                                <Alert variant={this.state.alertVariant} onClose={this.handleAlertClose} dismissible>
+                                                    {this.state.alertMessage}
+                                                </Alert>
+                                            </Col>
+                                        </Row>
+                                    )}
                                     <span style={{whiteSpace: "pre-line"}}>{this.props.description}</span>
                                     <br/>
                                     <br/>
@@ -56,11 +88,29 @@ export class TicketLine extends React.Component {
                                     <Button
                                         variant="success"
                                         size="sm"
-                                        onClick={() => this.handleResolveAndOpenRecord()}
+                                        onClick={() => this.handleResolveIssue()}
                                     >
                                         Resolve ticket and set record to "open" status
                                     </Button>
                                     }
+
+                                    {/* Modal */}
+                                    <Modal show={this.state.showConfirmModal} onHide={this.handleCancelModal} size={'lg'} centered>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Confirm Action</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            Are you sure you want to resolve ticket and set record to <b>open</b> status in project <b>{this.props.projectName}</b>?
+                                        </Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={this.handleCancelModal}>
+                                                Cancel
+                                            </Button>
+                                            <Button variant="danger" onClick={this.handleResolveAndOpenRecord} disabled={this.state.isProcessing}>
+                                                {this.state.isProcessing ? "Processing..." : "Confirm"}
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Modal>
 
                                 </div>
                             </Col>
